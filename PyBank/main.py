460f805f61da0a85ae9cf39a54647ce5880bc13a
mvsayewich <mvsayewich@gmail.com>
2018-11-03 10:00:09 -0400

    Updated Assignment

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,64 @@
+import os
+import csv
 
+# Path to collect data from the folder
+pybank = os.path.join('budget_data.csv')
+    
+# Read in the CSV file
+with open(pybank, 'r', newline = '') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    #This skips the first row of the CSV file.
+    next(csvreader, None)  
+    months = 0
+    total_profit = 0
+    monthly_change = []
+    pre_revenue = 0
+    average_change = 0
+    greatest_increase = 0
+    greatest_month = ""
+    lowest_increase = 0
+    lowest_month = ""
+    month = []
+    amount = []
+    output = ""
+    for line in csvreader:
+        month.append(line[0])
+        amount.append(int(line[1])) 
+        #print (line)
+        #The total number of months included in the dataset
+        #row_count = sum(1 for row in csvfile)
+        months = months + 1
+        #The total net amount of "Profit/Losses" over the entire period
+        total_profit = total_profit + int(line[1])
+        #The average change in "Profit/Losses" between months over the entire period
+        diff = int(line[1]) - pre_revenue
+        monthly_change.append(diff)
+        #The greatest increase in profits (date and amount) over the entire period
+        #greatest_increase = max(monthly_change) 
+        #The greatest decrease in profits (date and amount) over the entire period
+        #lowest_increase = min(monthly_change)
+#The average change in "Profit/Losses" between months over the entire period
+monthly_change = monthly_change[1:]
+average_change = sum(monthly_change) / len(monthly_change)
+#use the index value of greatest_increase to find the month
+greatest_increase = max(amount)
+greatest_month = month[amount.index(greatest_increase)] 
+#use the index value of lowest_increase to find the month
+lowest_increase = min(amount)
+lowest_month = month[amount.index(lowest_increase)] 
+
+print("Financial Analysis")
+print("-----")
+print("There are " + str(months) + " months in this dataset.")     
+print("The total profits are $" + str(total_profit) + ".")
+print("The average change is $" + str(average_change) + ".")
+print("The greatest increase in profits is $" + str(greatest_increase) + " in " + (greatest_month) + ".")
+print("The lowest increase in profits is $" + str(lowest_increase) + " in " + (lowest_month) + ".")
+
+#Export a text file with the results
+file = open("pybankscript.txt","w")
+file.write("Financial Analysis"'\n' "-----" '\n'"There are " + str(months) + " months in this dataset."
+          '\n' "The total profits are $" + str(total_profit) + "." '\n' "The average change is $" + str(average_change) + "."
+          '\n'"The greatest increase in profits is $" + str(greatest_increase) + " in " + (greatest_month) + "." 
+          '\n'"The lowest increase in profits is $" + str(lowest_increase) + " in " + (lowest_month) + ".")
+file.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,65 @@
+import os
+import csv
 
+# Path to collect data from the folder
+electdata = os.path.join('election_data.csv')
+
+
+total_votes_cast = 0
+list_of_candidates = []
+candidate_vote_counter = {}
+winner_name = ""
+winner_votes = 0 
+
+# Read in the CSV file
+with open(electdata, 'r', newline = '') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    #This skips the first row of the CSV file.
+    next(csvreader, None)  
+
+    for line in csvreader:
+        #The total number of votes cast included in the dataset    
+        total_votes_cast = total_votes_cast + 1
+        
+        #A complete list of candidates who received votes
+        current_candidate = line[2]
+        if current_candidate not in list_of_candidates:
+            list_of_candidates.append(current_candidate)
+            candidate_vote_counter[current_candidate] = 0
+        else:
+            candidate_vote_counter[current_candidate] = candidate_vote_counter[current_candidate] + 1    
+        #print(list_of_candidates)    
+        #The total number of votes each candidate won
+        #print(candidate_vote_counter)
+
+
+print(str(total_votes_cast) + " votes were cast.")
+print("Here is our complete list of candidates:" + str(list_of_candidates))    
+print("Here is the total of their votes: " + str(candidate_vote_counter))
+print("Here is the list of candidates and their vote percentage:")        
+
+for candidate in candidate_vote_counter:
+        
+    candidate_votes = candidate_vote_counter.get(candidate)
+    #print(candidate_votes)
+        
+    print(candidate)
+    percentageofvotes = ((candidate_votes / total_votes_cast) *100) 
+    print(percentageofvotes)
+        
+    #The winner of the election based on popular vote
+    if candidate_votes > winner_votes:
+        winner_votes = candidate_votes
+        winner_name = candidate
+            
+print("The winner of our election is: " + winner_name)
+
+#Export a text file with the results
+file = open("pypollscript.txt","w")
+file.write("Poll Analysis"'\n'"--------------"'\n'
+           + str(total_votes_cast) + " votes were cast." +
+           '\n'"The list of candidates are + str(list_of_candidates)" +
+           '\n'"Here is the total of their votes: " + str(candidate_vote_counter) +
+           '\n'"The vote percentage for Khan was 63%, Correy was 20%, Li was 14% and O'Tooley was 3%." +
+           '\n'"The winner of the election is: " + winner_name)
+file.close()
